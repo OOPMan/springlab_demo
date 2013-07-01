7c84100c4d3dc7ee21b9f8d5169c0ebd419defcc
Adam Jorgensen <adamj@sjsoft.com>
2013-07-01 20:51:02 +0200

    Configured Django URLs to serve Video resource

--- springlab_demo/urls.py
+++ springlab_demo/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, include, url
+from video_list.api import VideoResource
+
+video_resource = VideoResource()
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -14,4 +17,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    (r'^api/', include(video_resource.urls))
 )
