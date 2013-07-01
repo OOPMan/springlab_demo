6ea56ca04785ad6db5cd503eb5901c80ae27167f
Adam Jorgensen <adamj@sjsoft.com>
2013-07-01 19:00:23 +0200

    Initial commit of Django project files

---
+++ SpringLab_Demo/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'SpringLab_Demo.views.home', name='home'),
+    # url(r'^SpringLab_Demo/', include('SpringLab_Demo.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
