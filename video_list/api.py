e1c1eeee1f458fb1607a77d23588bf5c41fc22b3
Adam Jorgensen <adamj@sjsoft.com>
2013-07-01 20:56:01 +0200

    Confirm to PEP 8 Define allowed_methods Use TastyPie dummy authorization

--- video_list/api.py
+++ video_list/api.py
@@ -1,9 +1,13 @@
 __author__ = 'adamj'
 
+from tastypie.authorization import Authorization
 from tastypie.resources import ModelResource
 from video_list.models import Video
+
 
 class VideoResource(ModelResource):
     class Meta:
         queryset = Video.objects.all()
         resource_name = 'video'
+        allowed_methods = ['get', 'post', 'delete']
+        authorization = Authorization()  # TODO: Replace with secure Auth in real-world
