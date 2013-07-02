9f85909d6dcd859fe935193f382225a3f1eb95b1
Adam Jorgensen <adamj@sjsoft.com>
2013-07-02 23:25:53 +0200

    Configured Video store to retrieve video data from YouTube API following load of basic data from our server-side application and then trigger the VideoList view to refresh.

--- video_list/static/js/app/store/Video.js
+++ video_list/static/js/app/store/Video.js
@@ -10,6 +10,36 @@ Ext.define('VideoList.store.Video', {
     extend: 'Ext.data.Store',
     model: 'VideoList.model.Video',
     autoLoad: true,
+    youtubeIdPattern: /v=[a-zA-Z0-9]+/i,
+
+    listeners: {
+        load: function(self, records, successful, eOpts) {
+            if(!successful) return;
+            var idMap = {};
+            Ext.Array.forEach(records, function(record, index) {
+                var idPart = self.youtubeIdPattern.exec(record.data.url);
+                if(idPart.length == 1) idMap[idPart[0].substring(2)] = index;
+            });
+            if(idMap) {
+                gapi.client.load('youtube', 'v3', function() {
+                    var request = gapi.client.youtube.videos.list({
+                        part: 'snippet',
+                        id: Ext.Object.getKeys(idMap).join()
+                    });
+                    request.execute(function(response) {
+                        Ext.Array.forEach(response.items, function(item) {
+                            var recordIndex = idMap[item.id];
+                            if(recordIndex !== undefined) {
+                                records[recordIndex].data.title = item.snippet.title;
+                                records[recordIndex].data.description = item.snippet.description;
+                            }
+                        });
+                        Ext.ComponentQuery.query('VideoList').forEach(function(videoList) { videoList.refresh(); })
+                    });
+                });
+            }
+        }
+    },
 
     proxy: {
         type: 'rest',
