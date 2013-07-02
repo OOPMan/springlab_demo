95c659eda3d2ff1b01942f6d489c7c6d46300f13
Adam Jorgensen <adamj@sjsoft.com>
2013-07-02 21:37:41 +0200

    Updated VideoList view

--- video_list/static/js/app/view/Video/List.js
+++ video_list/static/js/app/view/Video/List.js
@@ -6,18 +6,21 @@
  * To change this template use File | Settings | File Templates.
  */
 
-Ext.define('SpringLabDemo.VideoList.view.Video.List', {
+Ext.define('VideoList.view.Video.List', {
     extend: 'Ext.view.View',
     alias: 'widget.VideoList',
     store: 'Video',
     tpl: new Ext.XTemplate(
-        '<tpl for=".">' +
-            '<span id="{id}">' +
-                '<span>Title:</span> {title}' +
-                '<span></span>' +
-            '</span><br/>' +
-        '</tpl>'),
-    itemSelector: 'span.newVideo',
-    emptyText: '<span class="newVideo">Click to add new Video</span>',
+        '<div>',
+            '<tpl for=".">',
+            '<span id="{id}" class="item">',
+                '<span>Title:</span> {title}<br/>',
+                '<span>Description:</span> {description}<br/>',
+                '<span>URL:</span> <a href="{url}">{url}</a><br/>',
+            '</span><br/>',
+            '</tpl>',
+        '</div>'),
+    itemSelector: 'span.item',
+    emptyText: '',
     data: []
 });
